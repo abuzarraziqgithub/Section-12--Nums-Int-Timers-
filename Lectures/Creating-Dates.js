6e02ad023928b6be679e393faf99e923be92bdfe
Abuzar RaziQ <abuzarraziqo@gmail.com>
2024-04-26 14:50:38 +0500

    DATES 1ST COMMIT

---
+++ Lectures/Creating-Dates.js
@@ -0,0 +1,9 @@
+"use strict";
+// WE CAN WRITE DATES IN FOUR WAYS:
+
+// CALLING DATE CONSTRUCTOR AND STORE IT INTO A VARIABLE.
+const now = new Date();
+console.log(now);
+// THIS TIME WILL NOT BE UPDATED
+console.log(new Date("Apr 26 2024 14:46:10"));
+console.log(new Date("December 24 2015 2:00"));
