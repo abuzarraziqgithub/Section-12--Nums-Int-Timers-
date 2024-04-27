5364194f4a4754ec699d664e031659b229ca449c
Abuzar RaziQ <abuzarraziqo@gmail.com>
2024-04-27 13:27:34 +0500

    2nd commit

--- Lectures/Internationalizing-Dates.js
+++ Lectures/Internationalizing-Dates.js
@@ -1,1 +1,27 @@
 "use strict";
+
+/*
+-> INTERNATIONALIZATION OF DATE IS USED TO CHANGE THE DATE FORMAT ACCORDING TO THE GIVEN COUNTRY OR LOCAL.
+
+
+*/
+
+// SYNTAX OF INT
+const now = new Date();
+console.log(now); // Sat Apr 27 2024 13:12:25 GMT+0500 (Pakistan Standard Time)
+
+const intDateUS = new Intl.DateTimeFormat("en-US").format(now);
+console.log(intDateUS); // 4/27/2024
+
+// DISPLAY THE LANGUAGE OF YOUR SYSTEM
+const locale = navigator.language;
+
+// OPTION OBJECT FOR DISPLAYING THE LOCALE DATE
+const Option = {
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+};
+
+const intDateEng = new Intl.DateTimeFormat(locale, Option).format(now);
+console.log(intDateEng);
