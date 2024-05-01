35017906ade142d788ef7ed30808cb7207411d68
Abuzar RaziQ <abuzarraziqo@gmail.com>
2024-05-01 09:43:19 +0500

    SET - INTERVAL - FUNCTION

--- Lectures/Timers-SetTimeOut-SetInterval.js
+++ Lectures/Timers-SetTimeOut-SetInterval.js
@@ -18,3 +18,18 @@ console.log("Waiting...");
 if (ingredients.includes("spinach")) clearTimeout(pizzaTimer);
 
 // THAT'S ABOUT SET TIME OUT FUNCTION, LET'S IMPLEMENT IT TO THE BANKIST PROJECT, TO SIMULATE APPROVAL OF OUR LOAN.
+
+// SET TIME INTERVAL:
+// THE SET TIME INTERVAL , EXECUTES CONTINOUSLY AFTER GIVEN TIME.
+
+// THE CALL BACK WILL EXECUTE AFTER EACH 2 SECONDS.
+const interval = setInterval(function () {
+  const now = new Date();
+  console.log(now);
+}, 2000);
+
+// IT WILL DELETE THE SET INERVAL FUNCTION
+clearInterval(interval);
+
+// A LITTLE CHALLENGE:
+// CREATE A REAL CLOCK , WHICH INCLUDE HOURS , MINUTES AND SECONDS USING SET INTERVAL.
